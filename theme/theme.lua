58d8dbfa6c27f6f5f234885581700b6c5e8aa80f
ben <rbjacikevicius@gmail.com>
2015-06-28 18:12:33 +0300

    changed color theme

--- theme/theme.lua
+++ theme/theme.lua
@@ -8,13 +8,13 @@ theme.font          = "sans 8"
 
 theme.bg_normal     = "#000000"
 theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#ff0000"
+theme.bg_urgent     = "#610000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
 
 theme.fg_normal     = "#aaaaaa"
 theme.fg_focus      = "#ffffff"
-theme.fg_urgent     = "#ffffff"
+theme.fg_urgent     = "#f75a52"
 theme.fg_minimize   = "#ffffff"
 
 theme.border_width  = 1
