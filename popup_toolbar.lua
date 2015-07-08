d8688c6f9171d72d30e21932134820c7b454ba46
ben <rbjacikevicius@gmail.com>
2015-07-09 02:42:16 +0300

    Popup toolbar

---
+++ popup_toolbar.lua
@@ -0,0 +1,92 @@
+local common = require("awful.widget.common")
+local wibox_bg = require("wibox.widget.background")
+local clock_widget = require("widgets.clock_big")
+local calendar_widget = require("widgets.calendar")
+
+local popup_toolbar = {}
+
+local mytasklist = {}
+mytasklist.buttons = awful.util.table.join(awful.button({}, 1, function(c)
+    if c == client.focus then
+        c.minimized = true
+    else
+        -- Without this, the following
+        -- :isvisible() makes no sense
+        c.minimized = false
+        if not c:isvisible() then
+            awful.tag.viewonly(c:tags()[1])
+        end
+        -- This will also un-minimize
+        -- the client, if needed
+        client.focus = c
+        c:raise()
+    end
+end),
+    awful.button({}, 3, function()
+        if instance then
+            instance:hide()
+            instance = nil
+        else
+            instance = awful.menu.clients({
+                theme = { width = 250 }
+            })
+        end
+    end),
+    awful.button({}, 4, function()
+        awful.client.focus.byidx(1)
+        if client.focus then client.focus:raise() end
+    end),
+    awful.button({}, 5, function()
+        awful.client.focus.byidx(-1)
+        if client.focus then client.focus:raise() end
+    end))
+
+
+local function list_update(w, buttons, label, data, objects)
+    common.list_update(w, buttons, label, data, objects)
+    w:set_max_widget_size(settings.popup_bar.tasklist.item_height)
+end
+
+function toggle_popup_box(s)
+    popup_toolbar[s].visible = not popup_toolbar[s].visible
+end
+
+local function render_popup_box(s)
+    popup_toolbar[s] = awful.wibox({
+        position = settings.popup_bar.position,
+        screen = s,
+        ontop = true,
+        width = settings.popup_bar.width,
+    })
+
+    popup_toolbar[s].visible = false
+
+    mytasklist[s] = awful.widget.tasklist(s,
+        awful.widget.tasklist.filter.currenttags,
+        mytasklist.buttons,
+        nil,
+        list_update,
+        wibox.layout.flex.vertical())
+
+
+    local top_layout = wibox.layout.fixed.vertical()
+    top_layout:add(clock_widget.widget)
+    top_layout:add(calendar_widget.widget)
+
+    local middle_layout = wibox.layout.flex.vertical()
+    middle_layout:add(mytasklist[s])
+
+    local layout = wibox.layout.align.vertical()
+    layout:set_top(top_layout)
+    layout:set_middle(middle_layout)
+
+    local background = wibox.widget.background(layout)
+
+    background:set_bgimage(beautiful.popup_toolbar_background)
+
+    popup_toolbar[s]:set_widget(background)
+end
+
+for s = 1, screen.count() do
+    render_popup_box(s)
+end
